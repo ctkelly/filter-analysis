315a6ad2cb5eea866d57baa384a34138e3943cf5
Chloe Kelly <chloetay3@gmail.com>
2021-06-09 19:17:09 -0400

    clarify comments

--- filter-use-analysis.py
+++ filter-use-analysis.py
@@ -1,42 +1,40 @@
-#import csv -- could not figure out how to make this work
-
 name = open("logs_chloe.csv", "r")
 dataset = name.read()
 
 datalist = dataset.split("\n")
-#print(len(datalist)) check that the number of lines that are output is correct -- 1417
+# print(len(datalist)) to check that the number of lines that are output is correct -- 1417
 
+# get rid of un-needed part of strings and append new strings to a list
 datalist2 = list()
 for item in datalist:
     newitem = item.replace("GET ,/matcher/filter-providers/?", "")
     datalist2.append(newitem)
-#print(datalist2)    
 
+# split string on the "&" and append filter pairs to a list
 pairlist = list()
 for item in datalist2:
     pairs = item.split("&")
     for pair in pairs:
         pairlist.append(pair)
-# print(pairlist) 
-       
+
+# split filter pairs on the "=" and append the items at [0] to a list       
 filterlist = list()
 for pair in pairlist:
     shortlist = pair.split("=")
     filterlist.append(shortlist[0])
-#print(filterlist)
 
+# count how many of each type of filter
 filtercounts = dict()
 for item in filterlist:
     filtercounts[item] = filtercounts.get(item, 0) + 1
-#print(filtercounts)
 
+# create tuples with value first and key second
 filtertups = list()
 for key, value in filtercounts.items():
     filtertups.append( (value, key) )
-#print(filtertups)    
 
+# sort in descending order
 sortedfilterlist = sorted(filtertups, reverse=True)
-#print(sortedfilterlist)
 
 for item in sortedfilterlist:
     print(item[0], item[1])
